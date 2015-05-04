e0d6b68af75835761d6d41f060b93a9cf0797e16
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-05-04 14:49:54 +0200

    based discret_marching-cubes.cxx on discret_marching_cubes05.cxx

--- discret_marching-cubes.cxx
+++ discret_marching-cubes.cxx
@@ -1,127 +1,147 @@
-//
-// GenerateModelsFromLabels
-//   Usage: GenerateModelsFromLabels InputVolume Startlabel Endlabel
-//          where
-//          InputVolume is a meta file containing a 3 volume of
-//            discrete labels.
-//          StartLabel is the first label to be processed
-//          EndLabel is the last label to be processed
-//          NOTE: There can be gaps in the labeling. If a label does
-//          not exist in the volume, it will be skipped.
-//
-//
+// program to apply vtkDiscreteMarchingCubes
+//01: based on discret_marching_cubes05.cxx
+
+
+
+
 #include <vtkMetaImageReader.h>
 #include <vtkImageAccumulate.h>
 #include <vtkDiscreteMarchingCubes.h>
 #include <vtkWindowedSincPolyDataFilter.h>
-#include <vtkMaskFields.h>
 #include <vtkThreshold.h>
 #include <vtkGeometryFilter.h>
 #include <vtkXMLPolyDataWriter.h>
 #include <vtkSmartPointer.h>
+#include <vtkImageConstantPad.h>
+#include <vtkMetaImageWriter.h>
+#include <vtkFeatureEdges.h>
+#include <vtkAppendPolyData.h>
+
+#include <vtkCallbackCommand.h>
+#include <vtkCommand.h>
 
 #include <vtkImageData.h>
 #include <vtkPointData.h>
 #include <vtkUnstructuredGrid.h>
 #include <vtksys/ios/sstream>
 
-int main (int argc, char *argv[]){
 
-    if (argc < 4){
-	cout << "Usage: " << argv[0] << " InputVolume StartLabel EndLabel" << endl;
+void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData){
+
+    vtkAlgorithm *filter= static_cast<vtkAlgorithm*>(caller);
+
+    switch(eventId){
+	case vtkCommand::ProgressEvent:
+	    fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetClassName(), 100.0 * filter->GetProgress());//stderr is flushed directly
+	    break;
+	case vtkCommand::EndEvent:
+	    std::cerr << std::endl << std::flush;   
+	    break;
+	}
+    }
+
+
+int main (int argc, char *argv[]){
+ 
+    if (argc != 5){
+	std::cerr << "Usage: " << argv[0] << " input output  StartLabel EndLabel" << std::endl;
 	return EXIT_FAILURE;
 	}
 
     // Create all of the classes we will need
     vtkSmartPointer<vtkMetaImageReader> reader =
 	vtkSmartPointer<vtkMetaImageReader>::New();
-    vtkSmartPointer<vtkImageAccumulate> histogram =
-	vtkSmartPointer<vtkImageAccumulate>::New();
     vtkSmartPointer<vtkDiscreteMarchingCubes> discreteCubes =
 	vtkSmartPointer<vtkDiscreteMarchingCubes>::New();
     vtkSmartPointer<vtkWindowedSincPolyDataFilter> smoother =
 	vtkSmartPointer<vtkWindowedSincPolyDataFilter>::New();
-    vtkSmartPointer<vtkThreshold> selector =
-	vtkSmartPointer<vtkThreshold>::New();
-    vtkSmartPointer<vtkMaskFields> scalarsOff =
-	vtkSmartPointer<vtkMaskFields>::New();
-    vtkSmartPointer<vtkGeometryFilter> geometry =
-	vtkSmartPointer<vtkGeometryFilter>::New();
+    vtkSmartPointer<vtkImageConstantPad> pad =
+        vtkSmartPointer<vtkImageConstantPad>::New();
+    vtkSmartPointer<vtkFeatureEdges> featureEdges =
+        vtkSmartPointer<vtkFeatureEdges>::New();
     vtkSmartPointer<vtkXMLPolyDataWriter> writer =
 	vtkSmartPointer<vtkXMLPolyDataWriter>::New();
 
-    // Define all of the variables
-    unsigned int startLabel = atoi(argv[2]);
-    unsigned int endLabel = atoi(argv[3]);
-    std::string filePrefix = "Label";
-    unsigned int smoothingIterations = 15;
-    double passBand = 0.001;
-    double featureAngle = 120.0;
+    vtkSmartPointer<vtkCallbackCommand> eventCallbackVTK = vtkSmartPointer<vtkCallbackCommand>::New();
+    eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
 
-    // Generate models from labels
-    // 1) Read the meta file
-    // 2) Generate a histogram of the labels
-    // 3) Generate models from the labeled volume
-    // 4) Smooth the models
-    // 5) Output each model into a separate file
+
+    // Define all of the variables
+    unsigned int startLabel = atoi(argv[3]);
+    unsigned int endLabel = atoi(argv[4]);
+    vtkstd::string filePrefix = argv[2]; //"Label";
+    unsigned int smoothingIterations = 20;
 
     reader->SetFileName(argv[1]);
+    reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    reader->Update();
 
-    histogram->SetInputConnection(reader->GetOutputPort());
-    histogram->SetComponentExtent(0, endLabel, 0, 0, 0, 0);
-    histogram->SetComponentOrigin(0, 0, 0);
-    histogram->SetComponentSpacing(1, 1, 1);
-    histogram->Update();
+    int *extent = reader->GetOutput()->GetExtent();
+    pad->SetInputData(reader->GetOutput());
+    pad->SetOutputWholeExtent(
+        extent[0] -1, extent[1] + 1,
+        extent[2] -1, extent[3] + 1,
+        extent[4] -1, extent[5] + 1);
+    pad->Update();
 
-    discreteCubes->SetInputConnection(reader->GetOutputPort());
+
+    discreteCubes->SetInputConnection(pad->GetOutputPort());
     discreteCubes->GenerateValues(
 	endLabel - startLabel + 1, startLabel, endLabel);
+    discreteCubes->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    discreteCubes->Update();
+
+    writer->SetInputData(discreteCubes->GetOutput());
+
+    vtksys_stl::stringstream ss2;
+    ss2 << filePrefix << "_raw.vtp";
+
+    writer->SetFileName(ss2.str().c_str());
+    writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    writer->Write();
+
+
+
+    ///check if surface is closed
+    std::cout << "Checking if surface is closed..." << std::endl;
+    featureEdges->FeatureEdgesOff();
+    featureEdges->BoundaryEdgesOn();
+    featureEdges->NonManifoldEdgesOff();
+    featureEdges->SetInputConnection(discreteCubes->GetOutputPort());
+    featureEdges->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    featureEdges->Update();
+
+    int numberOfEdges = featureEdges->GetOutput()->GetNumberOfCells();
+    if(numberOfEdges > 0)
+        std::cout << "Surface is not closed. # open edges: " << numberOfEdges << std::endl;
+    else
+        std::cout << "Surface is closed. # open edges: " << numberOfEdges << std::endl;
+
+    ///check if surface has non-manifold edges
+    std::cout << "Checking if surface has non-manifold edges..." << std::endl;
+    featureEdges->BoundaryEdgesOff();
+    featureEdges->NonManifoldEdgesOn();
+    featureEdges->Update();
+
+    numberOfEdges = featureEdges->GetOutput()->GetNumberOfCells();
+    if(numberOfEdges > 0)
+        std::cout << "Surface has non-manifold edges. # of non-manifold edges: " << numberOfEdges << std::endl;
+    else
+        std::cout << "Surface has only manifold edges. # of non-manifold edges: " << numberOfEdges << std::endl;
 
     smoother->SetInputConnection(discreteCubes->GetOutputPort());
     smoother->SetNumberOfIterations(smoothingIterations);
-    smoother->BoundarySmoothingOff();
-    smoother->FeatureEdgeSmoothingOff();
-    smoother->SetFeatureAngle(featureAngle);
-    smoother->SetPassBand(passBand);
     smoother->NonManifoldSmoothingOn();
     smoother->NormalizeCoordinatesOn();
+    smoother->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
     smoother->Update();
 
-    selector->SetInputConnection(smoother->GetOutputPort());
-    selector->SetInputArrayToProcess(0, 0, 0,
-	vtkDataObject::FIELD_ASSOCIATION_CELLS,
-	vtkDataSetAttributes::SCALARS);
+    vtksys_stl::stringstream ss;
+    ss << filePrefix << "_sws.vtp";
 
-    // Strip the scalars from the output
-    scalarsOff->SetInputConnection(selector->GetOutputPort());
-    scalarsOff->CopyAttributeOff(vtkMaskFields::POINT_DATA,
-	vtkDataSetAttributes::SCALARS);
-    scalarsOff->CopyAttributeOff(vtkMaskFields::CELL_DATA,
-	vtkDataSetAttributes::SCALARS);
-
-    geometry->SetInputConnection(scalarsOff->GetOutputPort());
-
-    writer->SetInputConnection(geometry->GetOutputPort());
-
-    for (unsigned int i = startLabel; i <= endLabel; i++){
-	// see if the label exists, if not skip it
-	double frequency =
-	    histogram->GetOutput()->GetPointData()->GetScalars()->GetTuple1(i);
-	if (frequency == 0.0){
-	    continue;
-	    }
-
-	// select the cells for a given label
-	selector->ThresholdBetween(i, i);
-
-	// output the polydata
-	vtksys_stl::stringstream ss;
-	ss << filePrefix << i << ".vtp";
-	cout << argv[0] << " writing " << ss.str() << endl;
-
-	writer->SetFileName(ss.str().c_str());
-	writer->Write();
-	}
+    writer->SetInputConnection(smoother->GetOutputPort());
+    writer->SetFileName(ss.str().c_str());
+    writer->Write();
 
     return EXIT_SUCCESS;
     }
