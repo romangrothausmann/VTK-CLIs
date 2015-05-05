a2cb77fb5b336f085bf36b4d1df66842da151cd7
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-05-05 14:01:54 +0200

    modifications based on ParallelIso.cxx example, segfaults partially

--- discrete_marching-cubes_MPI.cxx
+++ discrete_marching-cubes_MPI.cxx
@@ -1,8 +1,11 @@
 // program to apply vtkDiscreteMarchingCubes
-//01: based on discret_marching_cubes05.cxx
+//01: based on discrete_marching-cubes.cxx, Examples/ParallelProcessing/Generic/Cxx/ParallelIso.cxx and Examples/Infovis/Cxx/ParallelBFS.cxx
 
 
 
+#include <mpi.h>
+#include <vtkMPIController.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
 
 #include <vtkSmartPointer.h>
 #include <vtkMetaImageReader.h>
@@ -10,12 +13,17 @@
 #include <vtkImageConstantPad.h>
 #include <vtkDiscreteMarchingCubes.h>
 #include <vtkWindowedSincPolyDataFilter.h>
-#include <vtkXMLPolyDataWriter.h>
+#include <vtkXMLPPolyDataWriter.h>
 #include <vtkFeatureEdges.h>
 
 #include <vtkCallbackCommand.h>
 #include <vtkCommand.h>
 
+
+struct ParallelArgs{
+  int argc;
+  char** argv;
+};
 
 void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData){
 
@@ -32,17 +40,19 @@ void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* c
     }
 
 
-int main (int argc, char *argv[]){
+void processMPI(vtkMultiProcessController* controller, void* arg){
+    int argc; char **argv;
+    int myId, numProcs;
  
-    if (argc != 6){
-	std::cerr << "Usage: " << argv[0]
-		  << " input"
-		  << " output"
-		  << " StartLabel EndLabel"
-		  << " cap-surface"
-		  << std::endl;
-	return EXIT_FAILURE;
-	}
+    ParallelArgs* args = reinterpret_cast<ParallelArgs*>(arg);
+    argc= args->argc;
+    argv= args->argv;
+
+    // Obtain the id of the running process and the total
+    // number of processes
+    myId = controller->GetLocalProcessId();
+    numProcs = controller->GetNumberOfProcesses();
+
 
     // Create all of the classes we will need
     vtkSmartPointer<vtkMetaImageReader> reader =
@@ -55,8 +65,8 @@ int main (int argc, char *argv[]){
         vtkSmartPointer<vtkImageConstantPad>::New();
     vtkSmartPointer<vtkFeatureEdges> featureEdges =
         vtkSmartPointer<vtkFeatureEdges>::New();
-    vtkSmartPointer<vtkXMLPolyDataWriter> writer =
-	vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+    vtkSmartPointer<vtkXMLPPolyDataWriter> writer =
+	vtkSmartPointer<vtkXMLPPolyDataWriter>::New();
 
     vtkSmartPointer<vtkCallbackCommand> eventCallbackVTK = vtkSmartPointer<vtkCallbackCommand>::New();
     eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
@@ -69,8 +79,6 @@ int main (int argc, char *argv[]){
     unsigned int smoothingIterations = 20;
 
     reader->SetFileName(argv[1]);
-    reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    reader->Update();
 
     if(atoi(argv[5])){
 	int *extent = reader->GetOutput()->GetExtent(); //needs vtkImageData.h
@@ -79,7 +87,6 @@ int main (int argc, char *argv[]){
 	    extent[0] -1, extent[1] + 1,
 	    extent[2] -1, extent[3] + 1,
 	    extent[4] -1, extent[5] + 1);
-	pad->Update();
 	discreteCubes->SetInputConnection(pad->GetOutputPort());
 	}
     else
@@ -87,25 +94,32 @@ int main (int argc, char *argv[]){
 
     discreteCubes->GenerateValues(
 	endLabel - startLabel + 1, startLabel, endLabel);
-    discreteCubes->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    discreteCubes->Update();
-
-    writer->SetInputConnection(discreteCubes->GetOutputPort());
-
-    vtksys_stl::stringstream ss2;
-    ss2 << filePrefix << "_raw.vtp";
-
-    writer->SetFileName(ss2.str().c_str());
-    writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    writer->Write();
-
 
     smoother->SetInputConnection(discreteCubes->GetOutputPort());
     smoother->SetNumberOfIterations(smoothingIterations);
     smoother->NonManifoldSmoothingOn();
     smoother->NormalizeCoordinatesOn();
-    smoother->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    smoother->SetUpdateExtent(0, myId, numProcs, 0);
+
+    // // Tell the pipeline which piece we want to update.
+    // vtkStreamingDemandDrivenPipeline* exec =
+    // 	vtkStreamingDemandDrivenPipeline::SafeDownCast(smoother->GetExecutive());
+    // exec->SetUpdateNumberOfPieces(exec->GetOutputInformation(0), numProcs);
+    // exec->SetUpdatePiece(exec->GetOutputInformation(0), myId);
+
     smoother->Update();
+
+    writer->SetNumberOfPieces(numProcs);
+    writer->SetStartPiece(myId);
+    writer->SetEndPiece(myId);
+    writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+
+    vtksys_stl::stringstream ss2;
+    ss2 << filePrefix << "_raw.vtp";
+
+    writer->SetInputConnection(discreteCubes->GetOutputPort());
+    writer->SetFileName(ss2.str().c_str());
+    writer->Write();
 
     vtksys_stl::stringstream ss;
     ss << filePrefix << "_sws.vtp";
@@ -114,33 +128,42 @@ int main (int argc, char *argv[]){
     writer->SetFileName(ss.str().c_str());
     writer->Write();
 
+    }
 
-    ///check if surface is closed
-    std::cout << "Checking if surface is closed..." << std::endl;
-    featureEdges->FeatureEdgesOff();
-    featureEdges->BoundaryEdgesOn();
-    featureEdges->NonManifoldEdgesOff();
-    featureEdges->SetInputConnection(discreteCubes->GetOutputPort());
-    featureEdges->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    featureEdges->Update();
 
-    int numberOfEdges = featureEdges->GetOutput()->GetNumberOfCells();
-    if(numberOfEdges > 0)
-        std::cout << "Surface is not closed. # open edges: " << numberOfEdges << std::endl;
-    else
-        std::cout << "Surface is closed. # open edges: " << numberOfEdges << std::endl;
+int main (int argc, char *argv[]){
+ 
+    if (argc != 6){
+	std::cerr << "Usage: " << argv[0]
+		  << " input"
+		  << " output"
+		  << " StartLabel EndLabel"
+		  << " cap-surface"
+		  << std::endl;
+	return EXIT_FAILURE;
+	}
 
-    ///check if surface has non-manifold edges
-    std::cout << "Checking if surface has non-manifold edges..." << std::endl;
-    featureEdges->BoundaryEdgesOff();
-    featureEdges->NonManifoldEdgesOn();
-    featureEdges->Update();
+    // This is here to avoid false leak messages from vtkDebugLeaks when
+    // using mpich. It appears that the root process which spawns all the
+    // main processes waits in MPI_Init() and calls exit() when
+    // the others are done, causing apparent memory leaks for any objects
+    // created before MPI_Init().
+    MPI_Init(&argc, &argv);
 
-    numberOfEdges = featureEdges->GetOutput()->GetNumberOfCells();
-    if(numberOfEdges > 0)
-        std::cout << "Surface has non-manifold edges. # of non-manifold edges: " << numberOfEdges << std::endl;
-    else
-        std::cout << "Surface has only manifold edges. # of non-manifold edges: " << numberOfEdges << std::endl;
+    vtkSmartPointer<vtkMPIController> controller =
+	vtkSmartPointer<vtkMPIController>::New();
+    controller->Initialize(&argc, &argv, 1);
+
+    ParallelArgs args;
+    args.argc = argc;
+    args.argv = argv;
+
+    controller->SetSingleMethod(processMPI, &args);
+    controller->SingleMethodExecute();
+
+    controller->Finalize();
+    controller->Delete();
 
     return EXIT_SUCCESS;
     }
+
