668290da03be6a7322fd40f1dd381e83c0866a8e
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-01-13 18:09:34 +0100

    basic implementation of vtp2pvtp.cxx

--- vtp2pvtp.cxx
+++ vtp2pvtp.cxx
@@ -1,12 +1,13 @@
-////program for
+////program to write a VTP-file to a PVTP-file
 //01: based on template.cxx
 
 
 
 
 #include <vtkSmartPointer.h>
-#include <vtkMetaImageReader.h>
-#include <vtkXMLPolyDataWriter.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkExtractPolyDataPiece.h>//opposite of vtkDistributedDataFilter, use vtkDistributedDataFilter (D3) for more equally sized pieces
+#include <vtkXMLPPolyDataWriter.h>
 
 #include <vtkCallbackCommand.h>
 #include <vtkCommand.h>
@@ -40,22 +41,23 @@ void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* c
 
 int main (int argc, char *argv[]){
 
-    if (argc != 4){
+    if (argc != 5){
         std::cerr << "Usage: " << argv[0]
                   << " input"
                   << " output"
                   << " compress"
+                  << " #pieces"
                   << std::endl;
         return EXIT_FAILURE;
         }
 
-    if(!(strcasestr(argv[1],".mha") || strcasestr(argv[1],".mhd"))) {
-        std::cerr << "The input should end with .mha or .mhd" << std::endl;
+    if(!(strcasestr(argv[1],".vtp"))) {
+        std::cerr << "The output should end with .vtp" << std::endl;
         return -1;
         }
 
-    if(!(strcasestr(argv[2],".vtp"))) {
-        std::cerr << "The output should end with .vtp" << std::endl;
+    if(!(strcasestr(argv[2],".pvtp"))) {
+        std::cerr << "The output should end with .pvtp" << std::endl;
         return -1;
         }
 
@@ -64,26 +66,37 @@ int main (int argc, char *argv[]){
     eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
 
 
-    VTK_CREATE(vtkMetaImageReader, reader);
+    VTK_CREATE(vtkXMLPolyDataReader, reader);
     reader->SetFileName(argv[1]);
     reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    reader->Update();
+    reader->UpdateInformation();
 
-    VTK_CREATE(, filter);
-    filter->SetInputConnection(0, reader->GetOutputPort());
+    VTK_CREATE(vtkExtractPolyDataPiece, filter);
+    filter->SetInputConnection(reader->GetOutputPort());
     filter->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    filter->Update();
+    // filter->Update(); //this is called by writer
 
-    VTK_CREATE(vtkXMLPolyDataWriter, writer);
+    int numPieces= atoi(argv[4]);
+
+    VTK_CREATE(vtkXMLPPolyDataWriter, writer);
     writer->SetInputConnection(filter->GetOutputPort());
     writer->SetFileName(argv[2]);
+    writer->SetNumberOfPieces(numPieces);
+    //writer->SetGhostLevel(atoi(argv[5]));
     writer->SetDataModeToBinary();//SetDataModeToAscii()//SetDataModeToAppended()
     if(atoi(argv[3]))
         writer->SetCompressorTypeToZLib();//default
     else
         writer->SetCompressorTypeToNone();
     writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    writer->Write();
+
+    for(int i= 0; i < numPieces; i++){
+	//filter->UpdateInformation();
+	//filter->SetUpdateExtent(0, i, numPieces, 0);
+	writer->SetStartPiece(i);
+	writer->SetEndPiece(i);
+	writer->Write();
+	}
 
     return EXIT_SUCCESS;
     }
