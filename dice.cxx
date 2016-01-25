e85d3111755d77826fe8b86f8ac3a4ea33e08f41
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-01-25 13:12:58 +0100

    taking vtp2multi-piece_vtp.cxx as base for dice.cxx

---
+++ dice.cxx
@@ -0,0 +1,97 @@
+////program to write a VTP-file as a multi-piece VTP-file (that can be streamed)
+//01: based on vtp2pvtp.cxx
+
+
+
+
+#include <vtkSmartPointer.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkExtractPolyDataPiece.h>//opposite of vtkPolyDataStreamer, use vtkDistributedDataFilter (D3) for more equally sized pieces
+#include <vtkXMLPolyDataWriter.h>
+
+#include <vtkCallbackCommand.h>
+#include <vtkCommand.h>
+
+
+#define VTK_CREATE(type, name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+
+void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData){
+
+    vtkAlgorithm *filter= static_cast<vtkAlgorithm*>(caller);
+
+    switch(eventId){
+    case vtkCommand::ProgressEvent:
+        fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetClassName(), 100.0 * filter->GetProgress());//stderr is flushed directly
+        break;
+    case vtkCommand::EndEvent:
+        std::cerr << std::endl << std::flush;
+        break;
+
+        //// VTK does not throw errors (http://public.kitware.com/pipermail/vtkusers/2009-February/050805.html) use Error-Events: http://www.cmake.org/Wiki/VTK/Examples/Cxx/Utilities/ObserveError
+    case vtkCommand::ErrorEvent:
+        std::cerr << "Error: " << static_cast<char*>(callData) << std::endl << std::flush;
+        break;
+    case vtkCommand::WarningEvent:
+        std::cerr << "Warning: " << static_cast<char*>(callData) << std::endl << std::flush;
+        break;
+        }
+    }
+
+
+int main (int argc, char *argv[]){
+
+    if (argc != 5){
+        std::cerr << "Usage: " << argv[0]
+                  << " input"
+                  << " output"
+                  << " compress"
+                  << " #pieces"
+                  << std::endl;
+        return EXIT_FAILURE;
+        }
+
+    if(!(strcasestr(argv[1],".vtp"))) {
+        std::cerr << "The output should end with .vtp" << std::endl;
+        return -1;
+        }
+
+    if(!(strcasestr(argv[2],".vtp"))) {
+        std::cerr << "The output should end with .vtp" << std::endl;
+        return -1;
+        }
+
+
+    VTK_CREATE(vtkCallbackCommand, eventCallbackVTK);
+    eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
+
+
+    VTK_CREATE(vtkXMLPolyDataReader, reader);
+    reader->SetFileName(argv[1]);
+    reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    reader->UpdateInformation();
+
+    VTK_CREATE(vtkExtractPolyDataPiece, filter);
+    filter->SetInputConnection(reader->GetOutputPort());
+    filter->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    // filter->Update(); //this is called by writer
+
+    int numPieces= atoi(argv[4]);
+
+    VTK_CREATE(vtkXMLPolyDataWriter, writer);
+    writer->SetInputConnection(filter->GetOutputPort());
+    writer->SetFileName(argv[2]);
+    writer->SetNumberOfPieces(numPieces);
+    //writer->SetGhostLevel(atoi(argv[5]));
+    writer->SetDataModeToBinary();//SetDataModeToAscii()//SetDataModeToAppended()
+    if(atoi(argv[3]))
+        writer->SetCompressorTypeToZLib();//default
+    else
+        writer->SetCompressorTypeToNone();
+    writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    writer->Write();
+
+    return EXIT_SUCCESS;
+    }
+
+
