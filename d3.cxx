96529b4b7b7fc27cc15295c43e0a457038a3862f
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-01-27 16:48:10 +0100

    start to implement various decomposers for d3.cxx

--- d3.cxx
+++ d3.cxx
@@ -1,12 +1,18 @@
-////program to write a VTP-file as a multi-piece VTP-file (that can be streamed) using vtkKdTree (as in vtkDistributedDataFilter but no MPI dependence)
-//01: based on vtp2multi-piece_vtp.cxx
+////program to write a VTP-file as a multi-piece VTP-file (that can be streamed) using a tree-based spatial decomposition (e.g vtkKdTree as in vtkDistributedDataFilter but no MPI dependence)
+//01: based on vtp2multi-piece_vtp.cxx and http://www.vtk.org/Wiki/VTK/Examples/Cxx/DataStructures/DataStructureComparison
 
-
-
+//// ToDo:
+// - make extraction only dependent on vtkLocator functions, this will allow to also use e.g. vtkOctreePointLocator, vtkOBBTree, vtkModifiedBSPTree or vtkCellTreeLocator
+// -- sadly vtkLocator::GenerateRepresentation() results in vtkPolyData, would it yield a vtkUnstructuredGrid with 3D cells, these could be used to feed an extractor
 
 #include <vtkSmartPointer.h>
 #include <vtkXMLPolyDataReader.h>
+
 #include <vtkKdTree.h>
+#include <vtkOctreePointLocator.h> //there is no vtkOctree.h
+#include <vtkOBBTree.h> //arbitrary bounding box or oriented bounding box (OBB)
+#include <vtkModifiedBSPTree.h> //axis-aligned bounding box (AABB)
+ 
 #include <vtkExtractCells.h>
 #include <vtkIdList.h>
 #include <vtkDataSetSurfaceFilter.h>//faster version of vtkGeometryFilter
@@ -19,6 +25,7 @@
 
 #define VTK_CREATE(type, name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
 
+template<class TTree> int DoIt(int argc, char *argv[]);
 
 void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData){
 
@@ -45,12 +52,13 @@ void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* c
 
 int main (int argc, char *argv[]){
 
-    if (argc != 5){
+    if (argc != 6){
         std::cerr << "Usage: " << argv[0]
                   << " input"
                   << " output"
                   << " compress"
                   << " #pieces"
+                  << " decomposer-type"
                   << std::endl;
         return EXIT_FAILURE;
         }
@@ -65,6 +73,33 @@ int main (int argc, char *argv[]){
         return -1;
         }
 
+    int res= EXIT_FAILURE;
+    switch (atoi(argv[5])){
+    case 1:
+        res= DoIt<vtkKdTree>(argc, argv);
+        break;
+    // case 2:
+    //     res= DoIt<vtkOctreePointLocator>(argc, argv, locator);
+    //     break;
+    // case 3:
+    //     res= DoIt<vtkOBBTree>(argc, argv, locator);
+    //     break;
+    // case 4:
+    //     res= DoIt<vtkModifiedBSPTree>(argc, argv, locator);
+    //     break;
+    // case 5:
+    //     res= DoIt<vtkCellTreeLocator>(argc, argv, locator);
+    //     break;
+    default:
+        std::cerr << "Decomposer type not handled!" << std::endl;
+        break;
+        }//switch
+    return res;
+    }
+
+
+template<class TTree>
+int DoIt(int argc, char *argv[]){
 
     VTK_CREATE(vtkCallbackCommand, eventCallbackVTK);
     eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
@@ -75,7 +110,7 @@ int main (int argc, char *argv[]){
     reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
     reader->Update();
 
-    VTK_CREATE(vtkKdTree, d3);
+    VTK_CREATE(TTree, d3);
     d3->SetNumberOfRegionsOrMore(atoi(argv[4]));
     //d3->SetMinCells(); //min number of cells per region
     //d3->BuildLocatorFromPoints(reader->GetOutput()->GetPoints());
@@ -131,5 +166,4 @@ int main (int argc, char *argv[]){
 
     return EXIT_SUCCESS;
     }
-
 
