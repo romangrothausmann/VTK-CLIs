f39ef53b43497c5cda9dad11eebf0b69adedefb4
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-02-01 15:11:55 +0100

    basic implementation of dot2vtp.cxx

--- dot2vtp.cxx
+++ dot2vtp.cxx
@@ -1,11 +1,13 @@
-////program for
+////program to convert a graph stored in a DOT-file (graphviz) in a VTP
 //01: based on template.cxx
 
 
 
+#include <boost/graph/graphviz.hpp>
 
 #include <vtkSmartPointer.h>
-#include <vtkXMLPolyDataReader.h>
+#include <vtkMutableUndirectedGraph.h>
+#include <vtkGraphToPolyData.h>
 #include <vtkXMLPolyDataWriter.h>
 
 #include <vtkCallbackCommand.h>
@@ -37,6 +39,18 @@ void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* c
         }
     }
 
+// http://stackoverflow.com/questions/29898195/boostread-graphviz-how-to-read-out-properties
+// http://stackoverflow.com/questions/29496182/read-graphviz-in-boostgraph-pass-to-constructor/29501850#29501850
+struct DotVertex { 
+	std::string name;
+	std::string label;
+	std::string pos;
+	};
+
+struct DotEdge {
+    std::string label;
+    };
+
 
 int main (int argc, char *argv[]){
 
@@ -49,8 +63,8 @@ int main (int argc, char *argv[]){
         return EXIT_FAILURE;
         }
 
-    if(!(strcasestr(argv[1],".vtp"))) {
-        std::cerr << "The input should end with .vtp" << std::endl;
+    if(!(strcasestr(argv[1],".dot") || strcasestr(argv[1],".gv"))) {
+        std::cerr << "The input should end with .dot or .gv" << std::endl;
         return -1;
         }
 
@@ -63,14 +77,53 @@ int main (int argc, char *argv[]){
     VTK_CREATE(vtkCallbackCommand, eventCallbackVTK);
     eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
 
+    typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS, DotVertex, DotEdge> DotGraphT;
+    DotGraphT dotGraph;
 
-    VTK_CREATE(vtkXMLPolyDataReader, reader);
-    reader->SetFileName(argv[1]);
-    reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
-    reader->Update();
+    boost::dynamic_properties dp(boost::ignore_other_properties);
 
-    VTK_CREATE(, filter);
-    filter->SetInputConnection(0, reader->GetOutputPort());
+    dp.property("node_id",     boost::get(&DotVertex::name,        dotGraph));
+    dp.property("label",       boost::get(&DotVertex::label,       dotGraph));
+    dp.property("pos",         boost::get(&DotVertex::pos,         dotGraph));
+
+    dp.property("label",       boost::get(&DotEdge::label,         dotGraph));
+
+    std::ifstream fn(argv[1]);
+    boost::read_graphviz(fn, dotGraph, dp);
+   
+
+    vtkSmartPointer<vtkMutableUndirectedGraph> ograph= vtkSmartPointer<vtkMutableUndirectedGraph>::New();
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+
+    typedef boost::graph_traits<DotGraphT>::vertex_iterator vertex_iter;
+    std::pair<vertex_iter, vertex_iter> vp;
+    for(vp = vertices(dotGraph); vp.first != vp.second; ++vp.first){
+        //std::cout << dotGraph[*vp.first].pos << std::endl;
+        ograph->AddVertex();
+
+	std::vector<int> vect;
+	std::stringstream ss(dotGraph[*vp.first].pos);
+
+	double i;
+	while(ss >> i){
+	    vect.push_back(i);
+
+	    if(ss.peek() == ',')
+		ss.ignore();
+	    }
+    	points->InsertNextPoint(vect[0], vect[1], 0);
+        }
+
+    std::pair<DotGraphT::edge_iterator, DotGraphT::edge_iterator> edgeIteratorRange = boost::edges(dotGraph);
+    for(DotGraphT::edge_iterator edgeIterator = edgeIteratorRange.first; edgeIterator != edgeIteratorRange.second; ++edgeIterator){
+        ograph->AddEdge(boost::source(*edgeIterator, dotGraph), boost::target(*edgeIterator, dotGraph));
+        }
+
+    ograph->SetPoints(points);
+
+
+    VTK_CREATE(vtkGraphToPolyData, filter);
+    filter->SetInputData(ograph);
     filter->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
     filter->Update();
 
