b5da0e5d4f15317af433a436b11d9bfba2400bef
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-05-19 11:33:03 +0200

    taking read_VTPs_SDP.cxx as base for smooth-ws_SDP.cxx

---
+++ smooth-ws_SDP.cxx
@@ -0,0 +1,93 @@
+////program to test streamed reading of a VTP-file
+//01: based on template.cxx
+
+
+
+
+#include <vtkSmartPointer.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkInformation.h>
+#include <vtkPolyData.h>
+
+#include <vtkCallbackCommand.h>
+#include <vtkCommand.h>
+
+
+#define VTK_CREATE(type, name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+
+
+void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData){
+
+    vtkAlgorithm *filter= static_cast<vtkAlgorithm*>(caller);
+
+    switch(eventId){
+    case vtkCommand::ProgressEvent:
+        fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetClassName(), 100.0 * filter->GetProgress());//stderr is flushed directly
+        break;
+    case vtkCommand::EndEvent:
+        std::cerr << std::endl << std::flush;
+        break;
+
+        //// VTK does not throw errors (http://public.kitware.com/pipermail/vtkusers/2009-February/050805.html) use Error-Events: http://www.cmake.org/Wiki/VTK/Examples/Cxx/Utilities/ObserveError
+    case vtkCommand::ErrorEvent:
+        std::cerr << "Error: " << static_cast<char*>(callData) << std::endl << std::flush;
+        break;
+    case vtkCommand::WarningEvent:
+        std::cerr << "Warning: " << static_cast<char*>(callData) << std::endl << std::flush;
+        break;
+        }
+    }
+
+
+int main (int argc, char *argv[]){
+
+    if (argc != 3){
+        std::cerr << "Usage: " << argv[0]
+                  << " input"
+                  << " chunks"
+                  << std::endl;
+        return EXIT_FAILURE;
+        }
+
+    if(!(strcasestr(argv[1],".vtp"))) {
+        std::cerr << "The input should end with .vtp" << std::endl;
+        return -1;
+        }
+
+
+    VTK_CREATE(vtkCallbackCommand, eventCallbackVTK);
+    eventCallbackVTK->SetCallback(FilterEventHandlerVTK);
+
+
+    VTK_CREATE(vtkXMLPolyDataReader, reader);
+    reader->SetFileName(argv[1]);
+    reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    reader->UpdateInformation();
+
+    int numProcs= atoi(argv[2]);
+
+    vtkInformation* outInfo = reader->GetOutputInformation(0);
+    // Check if reader can handle piece requests (for unstructured) or sub-extents (for structured)
+    if (!outInfo->Get(vtkAlgorithm::CAN_HANDLE_PIECE_REQUEST()) &&
+	!outInfo->Get(vtkAlgorithm::CAN_PRODUCE_SUB_EXTENT())){
+	std::cout << "Reader cannot stream data!" << std::endl;
+	numProcs= 1;
+	}
+
+    unsigned long  totalPolyData= 0;
+    for(int myId= 0; myId < numProcs; myId++){
+        reader->SetUpdateExtent(0, myId, numProcs, 0);
+        reader->Update();
+
+        unsigned long  subtotalPolyData= reader->GetOutput()->GetNumberOfCells();
+        totalPolyData+= subtotalPolyData;
+
+        fprintf(stderr, "%5.1f%% (%d/%d): sub-total cells: %ld; accumulated total cells: %ld\n", (myId+1)*100.0/numProcs, myId+1, numProcs, subtotalPolyData, totalPolyData);
+        }
+
+    std::cout << "Total mesh cells: " << totalPolyData << std::endl;
+
+    return EXIT_SUCCESS;
+    }
+
+
