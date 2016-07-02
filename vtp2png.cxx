485818ac7b9eed7e1ad2bbd35d3fe6de5873b28c
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-07-02 13:39:03 +0200

    basic implementation of vtp2png.cxx

--- vtp2png.cxx
+++ vtp2png.cxx
@@ -1,12 +1,17 @@
-////program for
-//01: based on template.cxx
+////program to render VTPs to PNG
+//01: based on template.cxx and http://www.vtk.org/Wiki/VTK/Examples/Cxx/Utilities/Screenshot
 
 
 
 
 #include <vtkSmartPointer.h>
 #include <vtkXMLPolyDataReader.h>
-#include <vtkXMLPolyDataWriter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkPNGWriter.h>
 
 #include <vtkCallbackCommand.h>
 #include <vtkCommand.h>
@@ -40,11 +45,11 @@ void FilterEventHandlerVTK(vtkObject* caller, long unsigned int eventId, void* c
 
 int main (int argc, char *argv[]){
 
-    if (argc != 4){
+    if (argc != 5){
         std::cerr << "Usage: " << argv[0]
                   << " input"
                   << " output"
-                  << " compress"
+                  << " width height"
                   << std::endl;
         return EXIT_FAILURE;
         }
@@ -54,8 +59,8 @@ int main (int argc, char *argv[]){
         return -1;
         }
 
-    if(!(strcasestr(argv[2],".vtp"))) {
-        std::cerr << "The output should end with .vtp" << std::endl;
+    if(!(strcasestr(argv[2],".png"))) {
+        std::cerr << "The output should end with .png" << std::endl;
         return -1;
         }
 
@@ -69,20 +74,36 @@ int main (int argc, char *argv[]){
     reader->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
     reader->Update();
 
-    VTK_CREATE(, filter);
-    filter->SetInputConnection(0, reader->GetOutputPort());
+    VTK_CREATE(vtkPolyDataMapper, mapper);
+    mapper->SetInputConnection(reader->GetOutputPort());
+
+    VTK_CREATE(vtkActor, actor);
+    actor->SetMapper(mapper);
+
+    VTK_CREATE(vtkRenderer, renderer);
+    renderer->AddActor(actor);
+    renderer->SetBackground(1,1,1); // white
+
+    VTK_CREATE(vtkRenderWindow, renderWindow);
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetSize(atoi(argv[3]), atoi(argv[4]));
+    renderWindow->SetAlphaBitPlanes(1); // enable alpha channel
+    renderWindow->Render();
+
+    VTK_CREATE(vtkWindowToImageFilter, filter);
+    filter->SetInput(renderWindow);
+    // filter->SetMagnification(atof(argv[3])); // stiching is buggy, set renderWindow size instead
+    filter->SetInputBufferTypeToRGBA();
+    filter->ReadFrontBufferOff(); // read from the back buffer
+
     filter->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
     filter->Update();
 
-    VTK_CREATE(vtkXMLPolyDataWriter, writer);
+    VTK_CREATE(vtkPNGWriter, writer);
     writer->SetInputConnection(filter->GetOutputPort());
     writer->SetFileName(argv[2]);
-    writer->SetDataModeToBinary();//SetDataModeToAscii()//SetDataModeToAppended()
-    if(atoi(argv[3]))
-        writer->SetCompressorTypeToZLib();//default
-    else
-        writer->SetCompressorTypeToNone();
-    writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    // writer->SetCompressionLevel(atoi(argv[3]));
+    // writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK); // reports no progress
     writer->Write();
 
     return EXIT_SUCCESS;
