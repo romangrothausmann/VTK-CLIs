a01f9df87f0ce49cc6c6db8532c8f9dc42186a2d
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-10-22 12:46:38 +0200

    VTP header type set to 64 (not part of 6f40791c1)

--- marching-cubes.cxx
+++ marching-cubes.cxx
@@ -85,6 +85,7 @@ int main (int argc, char *argv[]){
     else
         writer->SetCompressorTypeToNone();
     writer->AddObserver(vtkCommand::AnyEvent, eventCallbackVTK);
+    writer->SetHeaderTypeToUInt64();
     writer->Write();
 
     return EXIT_SUCCESS;
